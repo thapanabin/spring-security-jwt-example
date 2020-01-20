fcc620b990a60b5aad0f1a17811a973dcfe75d5c
thapanabin <thapanabincr7@outlook.com>
2020-01-19 19:47:25 -0600

    added skeleton for spring-security-jwt-example

--- README.md
+++
@@ -1,1 +0,0 @@
-# spring-security-jwt-example
---
+++ springboot-security-jwt-example/src/main/java/com/nabin/SpringbootSecurityJwtExampleApplication.java
@@ -0,0 +1,13 @@
+package com.nabin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityJwtExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityJwtExampleApplication.class, args);
+	}
+
+}
